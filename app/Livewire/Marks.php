70d4f61499d18e7b93cf534fa55018e0a149c686
Rameez <121714501+rameezmuhammadh@users.noreply.github.com>
2024-10-29 11:10:19 +0530

    feat: created the marks class

---
+++ app/Livewire/Marks.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Marks extends Component
+{
+    public function render()
+    {
+        return view('livewire.marks');
+    }
+}
